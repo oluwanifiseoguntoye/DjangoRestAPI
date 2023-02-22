0814dbe7eb8bb0e6a0e1579609fcff9b2f616bd7
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-02-21 21:12:06 -0500

    product serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,14 +2,15 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from products.serializers import ProductSerializer
 
 # Create your views here.
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """Django Rest Framework API View"""
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data)
+    if instance:
+        data = ProductSerializer(instance).data
     return Response(data)
